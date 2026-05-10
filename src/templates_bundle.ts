54b8d0423dddb8318b2a7be9f9a2336f4468074c
Kevin <6215184+kevinkod@users.noreply.github.com>
2026-05-10 04:07:23 +0200

    chore: release v1.1.19

--- src/templates_bundle.ts
+++ src/templates_bundle.ts
@@ -2,7 +2,7 @@
 import type { CoreBundle } from "./domain/core_bundle.ts";
 import type { TemplateFile } from "./domain/template.ts";
 
-export const TEMPLATES_VERSION = "1.1.18";
+export const TEMPLATES_VERSION = "1.1.19";
 
 export const CORE_BUNDLE: CoreBundle = [
   {
